57d320b1ddb3bb139b9e6d3d5a9a3acf5cb50bfb
Gabriel Dos Santos <gabe.dos212@gmail.com>
2023-05-10 14:45:03 -0400

    Organizing testing suite structure

--- kv/test/extra_test.go
+++ kv/test/extra_test.go
@@ -443,3 +443,58 @@ func TestMultiSetIntegrationConcurrent(t *testing.T) {
 
 	setup.Shutdown()
 }
+
+// CAS TESTING
+
+func TestCAS(t *testing.T) {
+	setup := MakeTestSetup(MakeBasicOneShard())
+
+	_, wasFound, err := setup.NodeGet("n1", "abc")
+	assert.Nil(t, err)
+	assert.False(t, wasFound)
+
+	err = setup.NodeSet("n1", "abc", "123", 5*time.Second)
+	assert.Nil(t, err)
+
+	time.Sleep(1 * time.Second)
+
+	status, err := setup.NodeCAS("n1", "abc", "456", "123", 5*time.Second)
+	assert.Nil(t, err)
+	assert.True(t, status)
+
+	value, wasFound, err := setup.NodeGet("n1", "abc")
+	assert.Nil(t, err)
+	assert.True(t, wasFound)
+	assert.Equal(t, "456", value)
+
+}
+
+func TestCASTtl(t *testing.T) {
+	setup := MakeTestSetup(MakeBasicOneShard())
+
+	_, wasFound, err := setup.NodeGet("n1", "abc")
+	assert.Nil(t, err)
+	assert.False(t, wasFound)
+
+	err = setup.NodeSet("n1", "abc", "123", 5*time.Second)
+	assert.Nil(t, err)
+
+	time.Sleep(1 * time.Second)
+
+	status, err := setup.NodeCAS("n1", "abc", "456", "123", 5*time.Second)
+	assert.Nil(t, err)
+	assert.True(t, status)
+
+	value, wasFound, err := setup.NodeGet("n1", "abc")
+	assert.Nil(t, err)
+	assert.True(t, wasFound)
+	assert.Equal(t, "456", value)
+
+	time.Sleep(5 * time.Second)
+
+	value, wasFound, err = setup.NodeGet("n1", "abc")
+	assert.Nil(t, err)
+	assert.False(t, wasFound)
+	assert.Equal(t, "", value)
+
+}
